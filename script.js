3fd09d4c7c1f74e652659ab674513afcb85cfdca
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-10-15 20:40:38 +0200

    first styling perfect

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var aboutMe = document.getElementById("aboutMe")
 var mySkills = document.getElementById("mySkills")
 var myWork = document.getElementById("myWork")
 
-var positionAboutMe = 0
-var positionMySkills = 0
-var positionMyWork = 0
+var positionAboutMe = 3
+var positionMySkills = 3
+var positionMyWork = 3
 
-var myWorkDone = false
+var aboutMeDone = false
 var mySkillsDone = false 
 
-var positionMax = 91
-var positionBeforeMax = 85
+var positionMax = 94
+var positionBeforeMax = 88
 var increaseValue = 5
 var lastIncreaseValue = 6
 
@@ -25,71 +25,77 @@ if (getOS() === "Mac"){
     totalScrollsAfterMax = 40
 }
 
-console.log(getOS())
 
 addEventListener('wheel', (e) => {
 
-    if (positionMySkills == 0){ 
-        if (e.wheelDelta < 0 && positionMyWork < positionMax) {
-            if (positionMyWork == positionBeforeMax) {
-                positionMyWork += lastIncreaseValue
+    if (positionMySkills == 3){ 
+        if (e.wheelDelta < 0 && positionAboutMe < positionMax) {
+            if (positionAboutMe == positionBeforeMax) {
+                positionAboutMe += lastIncreaseValue
             } else {
-            positionMyWork += increaseValue 
+            positionAboutMe += increaseValue 
             }
-        } else if(e.wheelDelta > 0 && positionMyWork > 0) {
-            if (positionMyWork == positionMax) {
-                positionMyWork -= lastIncreaseValue
+        } else if(e.wheelDelta > 0 && positionAboutMe > 3) {
+            if (positionAboutMe == positionMax) {
+                positionAboutMe -= lastIncreaseValue
             } else {
-            positionMyWork -= increaseValue  
+            positionAboutMe -= increaseValue  
             }
         }
-        myWork.style.transform = "translateY("+ positionMyWork +"vw)"
+        // aboutMe.style.transform = "translateY("+ positionAboutMe +"vw)"
+        aboutMe.style.height = positionAboutMe + "vw"
     }
 
-    if (positionMyWork == positionMax && positionAboutMe == 0 && myWorkDone) {
+    if (positionAboutMe == positionMax && positionMyWork == 3 && aboutMeDone) {
         if (e.wheelDelta < 0 && positionMySkills < positionMax) {
             if (positionMySkills == positionBeforeMax) {
                 positionMySkills += lastIncreaseValue
             } else {
                 positionMySkills += increaseValue
             }
-        } else if(e.wheelDelta > 0 && positionMySkills > 0) {
+        } else if(e.wheelDelta > 0 && positionMySkills > 3) {
             if (positionMySkills == positionMax) {
                 positionMySkills -= lastIncreaseValue
             } else {
             positionMySkills -= increaseValue  
             }
         } 
-    mySkills.style.transform = "translateY("+ positionMySkills +"vw)"
+    // mySkills.style.transform = "translateY("+ positionMySkills +"vw)"
+    mySkills.style.height = positionMySkills + "vw"
     }
 
     if (positionMySkills == positionMax && mySkillsDone) {
-        if (e.wheelDelta < 0 && positionAboutMe < positionMax) {
-            if (positionAboutMe == positionBeforeMax) {
-                positionAboutMe += lastIncreaseValue
+        if (e.wheelDelta < 0 && positionMyWork < positionMax) {
+            if (positionMyWork == positionBeforeMax) {
+                positionMyWork += lastIncreaseValue
             } else {
-                positionAboutMe += increaseValue
+                positionMyWork += increaseValue
             }
-        } else if(e.wheelDelta > 0 && positionAboutMe > 0) {
-            if (positionAboutMe == positionMax) {
-                positionAboutMe -= lastIncreaseValue
+        } else if(e.wheelDelta > 0 && positionMyWork > 3) {
+            if (positionMyWork == positionMax) {
+                positionMyWork -= lastIncreaseValue
             } else {
-            positionAboutMe -= increaseValue  
+            positionMyWork -= increaseValue  
             }
         } 
-    aboutMe.style.transform = "translateY("+ positionAboutMe +"vw)"
+    // myWork.style.transform = "translateY("+ positionMyWork +"vw)"
+    myWork.style.height = positionMyWork + "vw"
     }
     
-    if (positionMyWork == positionMax) {
-        if (e.wheelDelta < 0 && positionMySkills === 0) {
+    if (positionAboutMe == positionMax) {
+        if (e.wheelDelta < 0 && positionMySkills === 3) {
             scrollAfterMax++   
         }
         if (scrollAfterMax === totalScrollsAfterMax ) {
-            myWorkDone = true 
+            aboutMeDone = true 
             scrollAfterMax = 0
+            aboutMe.style.position = "fixed"
+            aboutMe.style.bottom = 0
         }
     } else {
-        myWorkDone = false
+        aboutMeDone = false
+        aboutMe.style.position = "unset"
+        aboutMe.style.bottom = "unset"
     }
 
     if (positionMySkills == positionMax) {
@@ -97,11 +103,15 @@ addEventListener('wheel', (e) => {
             scrollAfterMax++
         }
         if (scrollAfterMax === totalScrollsAfterMax) {
-           mySkillsDone = true 
-           scrollAfterMax = 0
+            mySkillsDone = true 
+            scrollAfterMax = 0
+            mySkills.style.position = "fixed"
+            mySkills.style.bottom = "3vw"
         } 
     } else { 
         mySkillsDone = false
+        mySkills.style.position = "unset"
+        mySkills.style.bottom = "unset"
     }
 
     if (e.wheelDelta > 0){
@@ -119,7 +129,6 @@ function getOS() {
         iosPlatforms = ['iPhone', 'iPad', 'iPod'],
         os = null;
   
-        console.log(platform)
     if (macosPlatforms.indexOf(platform) !== -1) {
       os = 'Mac';
     } else if (iosPlatforms.indexOf(platform) !== -1) {
